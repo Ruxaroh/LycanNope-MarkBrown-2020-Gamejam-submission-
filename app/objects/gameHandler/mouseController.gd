fa5a485f57253d46b58d6577008be7396406c15c
Ruxaroh <60178948+Ruxaroh@users.noreply.github.com>
2020-07-11 22:51:05 +0100

    Glass Beta

--- app/objects/gameHandler/mouseController.gd
+++ app/objects/gameHandler/mouseController.gd
@@ -8,13 +8,14 @@ func _process(delta):
 	if grid.isWithinGrid(grid.world_to_map(mousePos)):
 		var CellHover = get_node("../grid/CellHover")
 		CellHover.position = grid.map_to_world(grid.world_to_map(mousePos))
-		if grid.collectedPotion == 0:
-			CellHover.modulate = Color(1,1,1)
-		elif grid.collectedPotion == 1:
+		if grid.collectedPotion == 1:
+			CellHover.visible = true
 			CellHover.modulate = Color(1,0,0)
 		elif grid.collectedPotion == 2:
+			CellHover.visible = true
 			CellHover.modulate = Color(0,1,0)
 		elif grid.collectedPotion == 3:
+			CellHover.visible = true
 			CellHover.modulate = Color(0.2,0.2,0.2)
 		checkPosValid(CellHover)
 
@@ -28,5 +29,5 @@ func checkPosValid(cellHover):
 	var cellPos = grid.world_to_map(cellHover.position)
 	if !(grid.get_node("wallTiles").get_cellv(cellPos) == -1 && grid.grid[cellPos.x][cellPos.y] == null):
 		cellHover.visible = false
-	else:
+	elif grid.collectedPotion != 0:
 		cellHover.visible = true
