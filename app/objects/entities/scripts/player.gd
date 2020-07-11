96f7a1803509f0efd86cb55e667f2185697bee35
Ruxaroh <60178948+Ruxaroh@users.noreply.github.com>
2020-07-11 16:13:50 +0100

    Enitial Wearwolf Movement
    Will now track NPC's when in sight

--- app/objects/entities/scripts/player.gd
+++ app/objects/entities/scripts/player.gd
@@ -7,6 +7,9 @@ var moveDir = Vector2()
 var grid
 var type = 1
 
+var animStart := false
+var animDone := false
+
 func _ready():
 	grid = get_parent()
 	set_physics_process(false)
@@ -17,20 +20,35 @@ func playerTurn():
 	set_physics_process(true)
 
 func _physics_process(delta):
-	if heroTurn:
-		moveDir = $heroMovement.getMoveDir()
-	else:
-		moveDir = $villainMovement.getMoveDir()
 	
-	if moveDir != Vector2.ZERO:		
-		if grid.isCellVacent(position, moveDir):
-			position = grid.updateChildPos(self)
-			set_physics_process(false)
-			emit_signal("playerTurnFinished")
+	var foundTarget = grid.findNpcInRange(self)
+	
+	if foundTarget == null:
+		moveDir = $heroMovement.getMoveDir()
+		if moveDir != Vector2.ZERO:		
+			if grid.isCellVacent(position, moveDir):
+				position = grid.updateChildPos(self)
+				set_physics_process(false)
+				emit_signal("playerTurnFinished")
+	else:
+		if !animDone && !animStart:
+			transform()
+			animStart = true
+		elif !animDone:
+			pass
+		else:
+			if grid.isCellVacent(position, foundTarget):
+				moveDir = foundTarget
+				position = grid.updateChildPos(self)
+				#set_physics_process(false)
 
 func transform():
 	$transformParticle.emitting = true
 	$transformDelay.start()
 
 func _on_transformDelay_timeout():
-	$playerSprite.play("villain")
+	if $playerSprite.animation == "hero":
+		$playerSprite.play("villain")
+		$transformDelay.start()
+	else:
+		animDone = true
--- app/objects/gameHandler/grid.gd
+++ app/objects/gameHandler/grid.gd
@@ -37,7 +37,27 @@ func updateChildPos(child):
 	
 	var targetPos = map_to_world(newGridPos)
 	return targetPos
-
-
-			
+	
 	return(Vector2(0,0))
+
+
+func findNpcInRange(child):
+	var childPos = world_to_map(child.position)
+	
+	for x in range(childPos.x, gridSize.x):
+		if grid[x][childPos.y] == 4:
+			return(Vector2(1, 0))
+	
+	for x in range(childPos.x, 0, -1):
+		if grid[x][childPos.y] == 4:
+			return(Vector2(-1, 0))	
+	
+	for y in range(childPos.y, gridSize.y):
+		if grid[childPos.x][y] == 4:
+			return(Vector2(0, 1))	
+	
+	for y in range(childPos.y, 0, -1):	
+		if grid[childPos.x][y] == 4:
+			return(Vector2(-1, 0))
+
+	return(null)
