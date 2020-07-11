8c4f050370cbfbebda3e3faaa0a4cdb0a77bf139
Dunelm1 <37511371+Dunelm1@users.noreply.github.com>
2020-07-11 23:12:18 +0100

    Fancy glass shading

--- app/objects/entities/scripts/glass.gd
+++ app/objects/entities/scripts/glass.gd
@@ -3,13 +3,7 @@ extends KinematicBody2D
 
 var type = 6
 var player = get_parent().get_node("player")
-connect("glassSmashed", turnController, "turnControl")
+#connect("glassSmashed", turnController, "turnControl")
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func
