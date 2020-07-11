22b19f627ab15090a40aca8bbe5357f6d1038f09
Hexallion <39310789+Hexallion@users.noreply.github.com>
2020-07-11 21:44:17 +0100

    VERY BASIC Cell selector

--- app/objects/gameHandler/mouseController.gd
+++ app/objects/gameHandler/mouseController.gd
@@ -1,14 +1,18 @@
 extends Node
 
+onready var grid = get_node("../grid")
+
 # Will be used for cell highlighting
 func _process(delta):
-	return
-	var mousePos = get_node("../").get_global_mouse_position()
-	print("Mouse Motion at: ", mousePos)
+	var mousePos = grid.get_global_mouse_position()
+	if grid.isWithinGrid(grid.world_to_map(mousePos)):
+		var CellHover = get_node("../grid/CellHover")
+		CellHover.position = grid.map_to_world(grid.world_to_map(mousePos))
+		print("Mouse Motion at: ", mousePos)
 
 
 func _input(event):
 	# Checks if event is mouse click
 	if event is InputEventMouseButton:
 		print("Mouse Click/Unclick at: ", event.position)
-		print(get_node("../grid").isCellVacent(event.position, Vector2(0,0), "mouse"))
+		print(grid.isCellVacent(event.position, Vector2(0,0), "mouse"))
