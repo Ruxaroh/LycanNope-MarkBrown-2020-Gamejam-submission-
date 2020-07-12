57eba711e58b5c671a650eafc42d74c8f3fd0907
Ruxaroh <60178948+Ruxaroh@users.noreply.github.com>
2020-07-12 10:29:50 +0100

    Fixed Level Restart
    It is no longer a game restart

--- app/objects/entities/scripts/nextLevel.gd
+++ app/objects/entities/scripts/nextLevel.gd
@@ -4,4 +4,5 @@ extends Node
 func nextLevel(nextLevelCode,level):
 	get_parent().add_child(load("res://app/levels/" + str(nextLevelCode) + ".tscn").instance())
 	get_parent().remove_child(level)
+	get_parent().currentLevel = nextLevelCode
 	#get_parent().get_node("turnController").turnControl()
