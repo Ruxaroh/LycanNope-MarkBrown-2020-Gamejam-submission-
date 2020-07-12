8418d83c02928678e8b5a29d929db86a0054f6a6
Hexallion <39310789+Hexallion@users.noreply.github.com>
2020-07-12 08:34:17 +0100

    Slightly tidied mouseController

--- app/objects/gameHandler/mouseController.gd
+++ app/objects/gameHandler/mouseController.gd
@@ -9,39 +9,49 @@ var smokeScene = load("res://app/objects/entities/smoke.tscn")
 # Will be used for cell highlighting
 func _process(delta):
 	var mousePos = grid.get_global_mouse_position()
+	# Checks if mouse is within the grid
 	if grid.isWithinGrid(grid.world_to_map(mousePos)):
-		var CellHover = get_node("../grid/CellHover")
+		# Sets Cell Hover position in line with grid
+		var CellHover = grid.get_node("CellHover")
 		CellHover.position = grid.map_to_world(grid.world_to_map(mousePos))
+		
+		# Change colour depending on potion collected
 		if grid.collectedPotion == 1:
 			CellHover.modulate = Color(1,0,0)
 		elif grid.collectedPotion == 2:
 			CellHover.modulate = Color(0,1,0)
 		elif grid.collectedPotion == 3:
 			CellHover.modulate = Color(0.2,0.2,0.2)
+		
+		# Makes CellHover visible if a potion is collected and it can be placed
 		if checkPosValid(CellHover) && grid.collectedPotion in [1,2,3]:
 			CellHover.visible = true
 		else:
 			CellHover.visible = false
 
-
+# Function to enable placing potion effects
 func _input(event):
-	var newObj
+	var potionObj
 	if event is InputEventMouseButton:
+		# Check if mouse within grid
 		var mousePos = grid.get_global_mouse_position()
 		if grid.isWithinGrid(grid.world_to_map(mousePos)):
-			var CellHover = get_node("../grid/CellHover")
+			var CellHover = grid.get_node("CellHover")
 			var posStore = CellHover.position
+			# Check if posible to place potion
 			if checkPosValid(CellHover) && grid.collectedPotion in [1,2,3]:
+				# Places potion effect onto grid
 				var gridPos = grid.world_to_map(CellHover.position)
 				if grid.collectedPotion == 1:
-					newObj = meatScene.instance()
+					potionObj = meatScene.instance()
 				elif grid.collectedPotion == 2:
-					newObj = slimeScene.instance()
+					potionObj = slimeScene.instance()
 				elif grid.collectedPotion == 3:
-					newObj = smokeScene.instance()
-				newObj.position = posStore
-				grid.grid[gridPos.x][gridPos.y] = newObj.type
-				get_parent().get_node("grid").get_node("actors").add_child(newObj)
+					potionObj = smokeScene.instance()
+				# Sets position of effect in world
+				potionObj.position = posStore
+				grid.grid[gridPos.x][gridPos.y] = potionObj.type
+				grid.get_node("actors").add_child(potionObj)
 				grid.collectedPotion = 0
 			
 
